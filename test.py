110a328824c676726c0fd8eb81005a728d541144
Apoorv Parmar <apoorvparmar@Apoorvs-MacBook-Air.local>
2026-07-07 16:53:53 +0530

    Add 1st agent workflow

--- test.py
+++ test.py
@@ -1,4 +1,34 @@
 from tools.search_tool import search_tool
+from langchain.agents import create_agent
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import PromptTemplate
 
-response = search_tool('1 news in india in july 2026')
-print(response)
+# llm = OllamaLLM(model = 'gemma2:2b')
+
+template = """Plan a 500 words blog on a given topic. "
+        "Return ONLY valid JSON matching this schema:\n"
+        '{\n'
+        '  "blog_title": "<string>",\n'
+        '  "tasks": [{"id": <int>, "title": "<string>", "brief": "<string>"}]\n'
+        '}\n'
+        "The blog should have multiple sections. "
+        "Do not include any extra text or markdown—only JSON."""
+
+# prompt = PromptTemplate(
+#     input_variables=[
+#         "topic",
+#     ],
+#     template=template,
+# )
+
+agent = create_agent(model="ollama:qwen3:8b",
+    tools=[search_tool],
+    system_prompt=template
+)
+
+# agent_executor = AgentExecutor(
+#     agent=agent, tools=[search_tool], verbose=True, handle_parsing_errors=True
+# )
+
+result = agent.invoke({"messages": [{"role": "user", "content": "Ethanol in petrol in India"}]})
+print(result['content'])
--- tools/search_tool.py
+++ tools/search_tool.py
@@ -2,6 +2,7 @@ from langchain_community.tools import DuckDuckGoSearchResults
 
 
 def search_tool(query):
+    """Search DuckDuckGo for the given query and return results."""
     tool = DuckDuckGoSearchResults()
     response = tool.invoke(query)
     return response
